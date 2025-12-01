e27734aa9317ba81d145a43999c66c3339ed9ffc
ZhahlaM <zhahla.mohamed@lis.ac.uk>
2025-12-01 23:55:44 +0000

    groupby added, pyshiny updated and new markdowns added

--- app.py
+++ app.py
@@ -16,12 +16,17 @@ app_ui = ui.page_fluid(
         "How many litres of water do you drink a day?",
         min=0.5, max=5, value=1
     ),
+     ui.input_slider(
+        "stress" ,
+        "How would you rate your stress levels out of 10 (10 being the highest)?",
+        min=0, max=10, value=1
+    ),
     ui.output_text_verbatim("value"),
 )
 
 def server(input, output, session):
     @render.text
     def value():
-        return f"Sleep: {input.sleep()}, Mindfulness: {input.mindfulness()}, Hydration: {input.hydration()}"
+        return f"Sleep: {input.sleep()}, Mindfulness: {input.mindfulness()}, Hydration: {input.hydration()}, Stress: {input.stress()}"
 
 app = App(app_ui, server)
