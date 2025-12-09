57d1e2386e7c69fc9b9e7aa5cb6114f12283540b
ZhahlaM <zhahla.mohamed@lis.ac.uk>
2025-12-09 18:52:09 +0000

    pyshiny almost finished

--- app.py
+++ app.py
@@ -1,32 +1,83 @@
 from shiny import ui, render, App
+import seaborn as sns
+import pandas as pd
+import statsmodels.formula.api as smf
 
-app_ui = ui.page_fluid(
-    ui.input_slider(
-        "sleep",
+df = pd.read_csv('holistic_health_lifestyle_dataset.csv')
+
+f = 'Overall_Health_Score ~ Sleep_Hours + Alcohol + Stress_Level + Mindfulness + Hydration + Physical_Activity + Smoking '
+model = smf.ols(formula=f, data=df).fit()
+
+app_ui = ui.page_sidebar(
+    ui.sidebar(
+        ui.input_slider(
+        "Sleep_Hours",
         "How many hours of sleep do you get normally?",
-        min=0, max=10, value=5
-    ),
-    ui.input_slider(
-        "mindfulness",
+        min=0, max=10, value=5),
+        ui.input_slider(
+        "Mindfulness",
         "How many minutes of mindfulness do you practice a day?",
-        min=0, max=60, value=30
-    ),
-      ui.input_slider(
-        "hydration" ,
+        min=0, max=60, value=30),
+        ui.input_slider(
+        "Hydration" ,
         "How many litres of water do you drink a day?",
-        min=0.5, max=5, value=1
-    ),
-     ui.input_slider(
-        "stress" ,
+        min=0.5, max=5, value=1),
+        ui.input_slider(
+        "Stress_Level" ,
         "How would you rate your stress levels out of 10 (10 being the highest)?",
-        min=0, max=10, value=1
+        min=0, max=10, value=1),
+        ui.input_slider( 
+        "Physical_Activity",
+        "How many minutes of exercise do you do a day?",
+        min=0 , max= 120, value= 10),
+        ui.input_slider(
+        "Alcohol",
+        "How many units of alcohol do you drink in a week?",
+        min = 0, max = 20, value = 5),
+        ui.input_slider(
+        "Smoking", 
+        "How many cigarettes do you smoke a day?",
+        min = 0, max= 30, value = 1),
     ),
+    ui.h1("Your Health Score"),
+    ui.layout_columns(
+        ui.card(
+            ui.card_header("Distribution of Health Scores"),
+            ui.output_plot("plot")
+        )),
     ui.output_text_verbatim("value"),
+    
+    ui.input_slider("n", "Number of bins", 1, 100, 20), 
 )
 
+
 def server(input, output, session):
+    @output
+    @render.plot(alt="Where does your health score fall on others health scores?")  
+    def plot():  
+        ax = sns.histplot(data= df, x="Overall_Health_Score", bins=input.n())  
+        ax.set_title("Health Score Distribution")
+        ax.set_xlabel("Health Score")
+        ax.set_ylabel("Count")
+        
+        out_of_sample_data = pd.DataFrame([{
+        'Sleep_Hours': input.Sleep_Hours(), 
+        'Alcohol': input.Alcohol(),
+        'Mindfulness': input.Mindfulness(),
+        'Hydration' : input.Hydration(), 
+        'Physical_Activity' : input.Physical_Activity(),
+        'Smoking': input.Smoking(),
+        'Stress_Level': input.Stress_Level()}])
+        user_hs = model.predict(out_of_sample_data)
+        ax.axvline(user_hs[0], color= 'red')
+        return ax  
+
+    @output
     @render.text
     def value():
-        return f"Sleep: {input.sleep()}, Mindfulness: {input.mindfulness()}, Hydration: {input.hydration()}, Stress: {input.stress()}"
+        return f"Sleep: {input.Sleep_Hours()}, Mindfulness: {input.Mindfulness()}, Hydration: {input.Hydration()}, Stress: {input.Stress_Level()}, Exercise: {input.Physical_Activity()}, Alcohol: {input.Alcohol()}, Smoking: {input.Smoking()}"
 
+ 
+
+#App
 app = App(app_ui, server)
